0cf70019acd846462df82df6dd605d79f8c455ad
Anthony Lamping <lamping.ap@gmail.com>
2020-05-05 17:49:18 -0400

    remove sed to setup the config, use new env vars

--- start.sh
+++ start.sh
@@ -8,16 +8,15 @@ if [ -f "$CONFIG" ]; then
 else
     echo "Creating configuration file!"
     cd $APP_DIR
-    cp /var/www/html/config/configuration.sample.php /var/www/html/config/configuration.php
-    sed -i 's/$dbHost="localhost"/$dbHost="'"${DB_HOST}"'"/g' config/configuration.php && \
-    sed -i 's/$dbUser=""/$dbUser="'"${MYSQL_USER}"'"/g' config/configuration.php && \
-    sed -i 's/$dbPassword=""/$dbPassword="'"${MYSQL_PASSWORD}"'"/g' config/configuration.php && \
-    sed -i 's/$dbDatabase=""/$dbDatabase="'"${MYSQL_DATABASE}"'"/g' config/configuration.php
+    cp config/configuration.sample.php ${CONFIG}
 
-    #generating 32 character string for session password
-    SESSION_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
-    sed -i 's/$sessionpassword = "3evBlq9zdUEuzKvVJHWWx3QzsQhturBApxwcws2m"/$sessionpassword = "'"${SESSION_PASSWORD}"'"/g' config/configuration.php
+    if [ -z "$LEAN_SESSION_PASSWORD" ]; then
+        #generating 32 character string for session password
+        SESSION_PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w ${1:-32} | head -n 1)
+        sed -i 's/$sessionpassword = "3evBlq9zdUEuzKvVJHWWx3QzsQhturBApxwcws2m"/$sessionpassword = "'"${SESSION_PASSWORD}"'"/g' ${CONFIG}
+    fi
 
 fi
 
 /usr/bin/supervisord -c /etc/supervisor/conf.d/supervisord.conf
+
